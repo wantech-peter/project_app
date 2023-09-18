a2b711da868fd964b421b1064d6d5677286a0ca2
lwl021051 <32732137+wantech-peter@users.noreply.github.com>
2023-09-19 00:17:12 +0800

    add right

--- project_extend/models/project_project.py
+++ project_extend/models/project_project.py
@@ -10,6 +10,17 @@ class Project(models.Model):
     total_timesheet_hours = fields.Integer(
         compute='_compute_total_timesheet_hours', groups='hr_timesheet.group_hr_timesheet_user')
     type_ids = fields.Many2many(default=lambda self: self._get_default_type_common())
+    can_edit_body_lead = fields.Boolean('Can Edit Body', compute='_compute_can_edit_body')
+    can_edit_body_team_manager = fields.Boolean('Can Edit Body', compute='_compute_can_edit_body')
+
+    @api.depends_context('uid')
+    def _compute_can_edit_body(self):
+        is_lead_consultant = self.user_has_groups('project_extend.group_project_lead_consultant')
+        is_team_manager = self.user_has_groups('project_extend.group_project_team_manager')
+        for item in self:
+            # is_approver and sheet.employee_id.user_id != self.env.user
+            item.can_edit_body_lead = is_lead_consultant
+            item.can_edit_body_team_manager = is_team_manager
 
     def _get_default_type_common(self):
         ids = self.env["project.task.type"].search([("case_default", "=", True)])
